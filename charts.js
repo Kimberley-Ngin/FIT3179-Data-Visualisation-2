e93b6003d4508a224b161f86d1844f33d725f61e
Kimberley Ngin <kimberleyngin@192-168-1-30.tpgi.com.au>
2024-10-09 21:36:54 +1100

    fixing formatting

--- charts.js
+++ charts.js
@@ -1,27 +1,27 @@
 var vg_1 = "js/active_fires_view.vg.json";
-vegaEmbed("#fire_spots_map", vg_1).then(function(result) {
+vegaEmbed("#fire_spots_map", vg_1, {"actions": false}).then(function(result) {
 }).catch(console.error);
 
 var vg_2 = "js/annual_rainfall.vg.json";
-vegaEmbed("#annual_rainfall", vg_2).then(function(result) {
+vegaEmbed("#annual_rainfall", vg_2, {"actions": false}).then(function(result) {
 }).catch(console.error);
 
 var vg_3 = "js/temperature_anomoly.vg.json";
-vegaEmbed("#temp_anomoly", vg_3).then(function(result) {
+vegaEmbed("#temp_anomoly", vg_3, {"actions": false}).then(function(result) {
 }).catch(console.error);
 
 var vg_4 = "js/annual_max_temp.vg.json";
-vegaEmbed("#max_temp", vg_4).then(function(result) {
+vegaEmbed("#max_temp", vg_4, {"actions": false}).then(function(result) {
 }).catch(console.error);
 
 var vg_5 = "js/proportion_of_forest_area_burnt.vg.json";
-vegaEmbed("#forest_area", vg_5).then(function(result) {
+vegaEmbed("#forest_area", vg_5, {"actions": false}).then(function(result) {
 }).catch(console.error);
 
 var vg_6 = "js/forest_category.vg.json";
-vegaEmbed("#forest_category", vg_6).then(function(result) {
+vegaEmbed("#forest_category", vg_6, {"actions": false}).then(function(result) {
 }).catch(console.error);
 
 var vg_7 = "js/isotype_grid.vg.json";
-vegaEmbed("#respiratory", vg_7).then(function(result) {
+vegaEmbed("#respiratory", vg_7,{"actions": false}).then(function(result) {
 }).catch(console.error);
